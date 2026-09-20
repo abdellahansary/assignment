7b434a1bf5bbfad74de796be26c6bff02341d015
Abdallah <137111757+abdellahansary@users.noreply.github.com>
2026-09-20 19:25:57 +0300

    Delete asgs directory

--- asgs/src/routes/logsRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { insertLog } = require("../controllers/logsController");
-
-router.post("/", insertLog);
-
-module.exports = router;
